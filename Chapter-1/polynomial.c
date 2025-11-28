5942f8720b3780ba9081c53ab08197f2850a1eb8
Soumyadeep <dassoumyadeep250@gmail.com>
2025-11-28 11:20:02 +0000

    Solved the same polynomial using Horner's Rule

--- Chapter-1/polynomial.c
+++ Chapter-1/polynomial.c
@@ -7,7 +7,7 @@ int main (void)
     float x;
     printf("Value of x: ");
     scanf("%f", &x);
-    float value=((3*x*x*x*x*x)+(2*x*x*x*x)-(5*x*x*x)-(x*x)+(7*x)-6);
+    float value= ((((3*x+2)*x-5)*x-1)*x+7)*x-6;
     printf("The value is: %.2f\n", value);
     return 0;
 }
