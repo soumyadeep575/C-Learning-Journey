51d5d4ac43c8a646b921adf1af9df01db622b276
Soumyadeep <dassoumyadeep250@gmail.com>
2026-01-10 14:28:38 +0000

    Wrote a program in C to calculate the number of digits in a number

---
+++ Chapter-4/digits.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int number,digits, number_1;
+    digits=0;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    number_1=number;
+    while (number_1 % 10 != 0)
+    {
+        digits++;
+        number_1=number_1/10;
+    }
+    printf("The number %d has %d digits\n", number, digits);
+}
