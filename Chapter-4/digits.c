617e3c12d1025d13d34025eab8bd6132e50c33d0
Soumyadeep <dassoumyadeep250@gmail.com>
2026-01-10 14:57:08 +0000

    Modified the program due to one mistake

--- Chapter-4/digits.c
+++ Chapter-4/digits.c
@@ -7,10 +7,14 @@ int main(void)
     printf("Enter a number: ");
     scanf("%d", &number);
     number_1=number;
-    while (number_1 % 10 != 0)
+    if (number==0)
+        digits=1;
+    else{
+    while (number_1 != 0)
     {
         digits++;
         number_1=number_1/10;
     }
+}
     printf("The number %d has %d digits\n", number, digits);
 }
